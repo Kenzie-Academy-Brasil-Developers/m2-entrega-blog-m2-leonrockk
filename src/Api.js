64f63823bc4477854e0b2dcfd69481eead691c17
Leonardo Rocha <leonard_rock@outlook.com>
2022-03-18 19:57:01 -0300

    create the skeleton of the javascript oop that will be implemented

---
+++ src/Api.js
@@ -0,0 +1,23 @@
+class Api {
+    static async cadastrarUsuario(){
+
+    }
+
+    static async fazerLogin(){
+
+    }
+
+    static async criarPostagem(){
+
+    }
+
+    static async editarPostagem(){
+
+    }
+
+    static async excluirPostagem(){
+
+    }
+
+
+}
